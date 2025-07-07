c5c01233eff49b387de46575456314e94a065672
Dreamchaser-aus <dreamchaseraus8@gmail.com>
2025-07-07 18:07:53 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -106,4 +106,7 @@ def main():
     app.run_polling()
 
 if __name__ == "__main__":
-    main()
+    init_tables()   # 自动建表
+    fix_token_type()  # ★★ 这里加上，自动修正 token 字段为整数
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
